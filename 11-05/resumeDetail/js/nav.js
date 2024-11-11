5ba3ae446d0725a52370344798cbd7b64fa6fcb0
snowman12320 <snowman12320@gmail.com>
2024-11-11 16:58:15 +0800

    新增 resumeDetail.html 檔案以整合自訂元素結構

---
+++ 11-05/resumeDetail/js/nav.js
@@ -0,0 +1,25 @@
+const aboutMeIcon = document.getElementById('aboutMeIcon');
+const aboutMeMenu = document.getElementById('aboutMeMenu');
+
+aboutMeIcon.addEventListener('click', function() {
+    if (this.src.includes('aboutMeIconbBlue.png')) {
+        this.src = './images/aboutMeIcon.png';
+    } else {
+        if(!window.location.href.includes('resume')){
+            this.src = './images/aboutMeIconbBlue.png';
+        }
+    }
+    
+    if (aboutMeMenu.style.display === 'none' || !aboutMeMenu.style.display) {
+        aboutMeMenu.style.display = 'block';
+    } else {
+        aboutMeMenu.style.display = 'none';
+    }
+});
+
+document.addEventListener('click', function(event) {
+    if (!aboutMeIcon.contains(event.target) && !aboutMeMenu.contains(event.target)) {
+        aboutMeMenu.style.display = 'none';
+        aboutMeIcon.src = './images/aboutMeIcon.png';
+    }
+});
