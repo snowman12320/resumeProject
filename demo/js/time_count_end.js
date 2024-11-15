fd35e843fc7329d00e5bbf31f6f62d72729d71b9
snowman12320 <snowman12320@gmail.com>
2024-11-15 18:00:20 +0800

    Stop tracking files in /demo/ directory

--- demo/js/time_count_end.js
+++
@@ -1,7 +0,0 @@
-time02 = performance.now();
-
-message_box.innerHTML = `\n<br>Consumed <span style="color: ${text_color}">${(time02 - time01).toFixed(3)}ms</span> to execute this <span style="color: ${text_color}">${type}</span> program.`;
-
-document.body.style.backgroundColor = 'Black';
-document.body.style.color = 'Gold';
-document.body.append(message_box);
